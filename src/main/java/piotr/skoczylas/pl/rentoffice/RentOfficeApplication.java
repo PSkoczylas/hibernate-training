551bb949c9191f62e058dd0df21c8f93c7a226e2
skoczylp <piotr.skoczylas@contractors.roche.com>
2018-05-31 14:34:55 +0200

    Initial repo

---
+++ src/main/java/piotr/skoczylas/pl/rentoffice/RentOfficeApplication.java
@@ -0,0 +1,12 @@
+package piotr.skoczylas.pl.rentoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentOfficeApplication.class, args);
+	}
+}
---
+++ src/test/java/piotr/skoczylas/pl/rentoffice/RentOfficeApplicationTests.java
@@ -0,0 +1,16 @@
+package piotr.skoczylas.pl.rentoffice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentOfficeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
